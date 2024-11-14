ec79b38e53ccd959233c9bf2c4c61e0a2541142a
rahmoncodes <abdulrahmonbb@gmail.com>
2024-11-14 01:19:51 +0100

    3-all.js v2

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
-export default function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-        .then((results) => {
-            const [ photoData, userData ] = results;
-            console.log(photoData.body, userData.firstName, userData.lastName);
-        })
-        .catch(() => console.log('Signup system offline'));
-};
+export default function handleProfileSignup () {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((results) => {
+      const [photoData, userData] = results;
+      console.log(photoData.body, userData.firstName, userData.lastName);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
