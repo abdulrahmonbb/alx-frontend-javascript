4ebe46763a5fd31bdc5af51185d708c6ecd5e973
rahmoncodes <abdulrahmonbb@gmail.com>
2024-11-24 23:22:55 +0100

    add: implemented a currency class

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,41 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      TypeError('Code must be a string');
+    }
+
+    this._code = code;
+
+    if (typeof name !== 'string') {
+      TypeError('Name must be a string');
+    }
+
+    this._name = name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(code) {
+    if (typeof code !== 'string') {
+      TypeError('Code must be a string');
+    }
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(name) {
+    if (typeof name !== 'string') {
+      TypeError('Name must be a string');
+    }
+    this._name = name;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
