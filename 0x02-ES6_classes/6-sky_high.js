3759bee34bd91006f21036da098364382801fd96
rahmoncodes <abdulrahmonbb@gmail.com>
2024-11-25 15:05:09 +0100

    add: implementation of 6-sky_high.js

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+
+    if (typeof floors !== 'number') {
+      throw new TypeError('floors must be a number');
+    }
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
